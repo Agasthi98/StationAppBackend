bb6ca966470b8becf325b02e96aa9eb6b8551890
@IT19124940 <it19124940@my.sliit.lk>
2022-10-28 17:10:08 +0530

    time get complete

--- controllers/timeController.js
+++ controllers/timeController.js
@@ -4,8 +4,8 @@ import TimeModel from '../models/timeModel.js'
 const addTime =  (req, res) => {
     var arrivalTime =req.body.arrivalTime;
     var endTime =req.body.endTime;
-    var stationNumber =req.body.stationName;
     var stationName =req.body.stationName;
+    var stationNumber =req.body.stationNumber;
     var stationLocation =req.body.stationLocation;
       
     console.log("Agasthi:"+arrivalTime,endTime,stationName,stationNumber,stationLocation)
@@ -26,4 +26,26 @@ const addTime =  (req, res) => {
       }
     }
 
-    export{addTime}
+    const getShedOpenTimeDetails = (req, res)=>{
+        var StationNumber =req.body.StationNumber;
+        console.log(StationNumber)
+        TimeModel.find({stationNumber: StationNumber})
+        .then((data) => {
+          console.log(data[0])
+          // //console.log("PETROL DETAILS"+data[0].liters+data.arrivalTime[0]+data.endTime[0]+data.queueLength[0]+data.stationNumber[0]+data.stationName+data.stationLocation[0]);
+          res.json({
+            arrivalTime:data[0].arrivalTime,
+            endTime:data[0].endTime,
+            stationNumber:data[0].stationNumber,
+            stationName:data[0].stationName,
+            stationLocation:data[0].stationLocation
+          })
+        }).catch((err) => {
+          console.log("GET SHED TIME FAILED"+err);
+          res.json({
+              code:400 ,
+              body: "Fail" 
+          })
+        });
+    }
+    export{addTime,getShedOpenTimeDetails}
--- routes/timeRoutes.js
+++ routes/timeRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const  router = express.Router()
-import { addTime } from '../controllers/timeController.js'
+import { addTime,getShedOpenTimeDetails } from '../controllers/timeController.js'
 
 //diesel add route
 router.post('/', addTime)
+router.post('/display', getShedOpenTimeDetails)
 
 
 export default router 
